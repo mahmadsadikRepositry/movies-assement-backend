9f8703aeb162a9489ff16fb75457f2fc38b2f44b
mahmadsadikRepositry <sadikkoliya@gmail.com>
2025-11-08 14:16:01 -0500

    Added HTTP Module with Axios for cross Server Communication, installed required package.

--- src/movie-list/movie-list.module.ts
+++ src/movie-list/movie-list.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MovieListService } from './movie-list.service';
 import { MovieListController } from './movie-list.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [MovieListController],
   providers: [MovieListService],
 })
